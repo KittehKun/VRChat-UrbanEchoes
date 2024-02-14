add9379c08ade68021b6a6dfc40fb97df727a2ae
Zach H <huntzachary30@gmail.com>
2024-02-13 18:43:50 -0600

    Added generic framework for PlayerAudio.

---
+++ Player/PlayerAudio.cs
@@ -0,0 +1,42 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+//This is a general purpose script for playing audio on the player.
+public class PlayerAudio : UdonSharpBehaviour
+{
+    //AudioSource Variables - Assigned in the Unity Editor
+    [SerializeField] private AudioSource playerFootstep;
+    [SerializeField] private AudioClip[] footstepSounds;
+
+	private readonly float _footstepCooldown = 0.5f;
+	private float _lastFootstepTime;
+
+	void Start()
+    {
+        if (!playerFootstep) Debug.LogError("Player Audio: No AudioSource assigned to playerFootstep");
+    }
+
+	public void Update()
+	{
+        //Move this object to the player's position
+        this.transform.position = Networking.LocalPlayer.GetPosition();
+        
+        //Check if player is the moving
+        if(Networking.LocalPlayer.GetVelocity() != Vector3.zero && Time.time >= _lastFootstepTime + _footstepCooldown)
+        {
+            PlayFootstep();
+
+			// Update the last footstep time
+			_lastFootstepTime = Time.time;
+		}
+	}
+
+    private void PlayFootstep()
+    {
+        //Play a footstep sound
+        playerFootstep.PlayOneShot(footstepSounds[Random.Range(0, footstepSounds.Length)]);
+    }
+}
