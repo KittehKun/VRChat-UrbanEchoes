431e62c1efc488e2c2da6bbd04f12ff259c3069b
Zach H <huntzachary30@gmail.com>
2024-03-18 15:47:52 -0500

    Finished basic pickup for library books

--- Interactables/LibraryPickup.cs
+++ Interactables/LibraryPickup.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
 public class LibraryPickup : UdonSharpBehaviour
 {
-    void Start()
+    [Header("Library Minigame")]
+    [SerializeField] private LibraryMinigame libraryMinigame;
+
+	private void Start()
+	{
+        transform.gameObject.SetActive(false);
+	}
+
+	public override void Interact()
     {
-        
+        libraryMinigame.BookCollected();
+        DisablePickup();
+    }
+
+    private void DisablePickup()
+    {
+        transform.gameObject.SetActive(false);
     }
 }
