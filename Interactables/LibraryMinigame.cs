85ee541dc47ce6c748005bdc855a8891409032e8
Zach H <huntzachary30@gmail.com>
2024-03-19 18:53:20 -0500

    Added missing SFX and documentation

--- Interactables/LibraryMinigame.cs
+++ Interactables/LibraryMinigame.cs
@@ -18,6 +18,7 @@ public class LibraryMinigame : UdonSharpBehaviour
 
 	[Header("Library SFX")]
 	[SerializeField] private AudioSource librarySFX; //Set in Unity Inspector | The audio source that will play all library audio clips.
+	[SerializeField] private AudioClip libraryStartSFX; //Set in Unity Inspector | The sound effect that will play when the player starts the library minigame.
 	[SerializeField] private AudioClip libraryCompleteSFX; //Set in Unity Inspector | The sound effect that will play when the player completes the library minigame.
 	[SerializeField] private AudioClip libraryFailedSFX; //Set in Unity Inspector | The sound effect that will play when the player fails the library minigame.
     
@@ -45,6 +46,9 @@ public class LibraryMinigame : UdonSharpBehaviour
 		}
 	}
 
+	/// <summary>
+	/// Starts the process of the library minigame. Disables the library mesh and collider, sets the player's on job status to true, and enables the task items.
+	/// </summary>
 	private void StartMinigame()
     {
 		//Start the timer
@@ -60,19 +64,31 @@ public class LibraryMinigame : UdonSharpBehaviour
 
 		//Enable the task items
 		EnableTaskItems();
+
+		//Play the start SFX
+		PlayStartSFX();
 	}
 
+	/// <summary>
+	/// Stops the timer. This method is called when the player completes the minigame or fails the minigame.
+	/// </summary>
 	private void StopMinigame()
 	{
 		timerStarted = false;
 	}
 
+	/// <summary>
+	/// Fails the minigame. Begins the process of resetting the minigame and returns the library to its default state.
+	/// </summary>
 	private void FailMinigame()
 	{
 		ResetMinigame();
 		PlayFailSFX();
 	}
 
+	/// <summary>
+	/// Resets the minigame and returns the library to its default state.
+	/// </summary>
 	private void ResetMinigame()
 	{
 		StopMinigame();
@@ -86,6 +102,9 @@ public class LibraryMinigame : UdonSharpBehaviour
 		playerStats.OnJob = false;
 	}
 
+	/// <summary>
+	/// Enables all the tasks items in random order in the Hierarchy.
+	/// </summary>
 	private void EnableTaskItems()
 	{
 		//Enable a random amount of task items based on child count
@@ -117,18 +136,45 @@ public class LibraryMinigame : UdonSharpBehaviour
 		playerHUD.UpdateJobTaskCount(booksToCollect);
 	}
 
+	/// <summary>
+	/// Plays the start sound effect.
+	/// </summary>
+	private void PlayStartSFX()
+	{
+		librarySFX.clip = libraryStartSFX;
+		librarySFX.Play();
+	}
+
+	/// <summary>
+	/// Plays the complete sound effect.
+	/// </summary>
 	private void PlayCompleteSFX()
 	{
 		librarySFX.clip = libraryCompleteSFX;
 		librarySFX.Play();
 	}
 
+	/// <summary>
+	/// Plays the failed sound effect.
+	/// </summary>
 	private void PlayFailSFX()
 	{
 		librarySFX.clip = libraryFailedSFX;
 		librarySFX.Play();
 	}
 
+	/// <summary>
+	/// This method is called by the LibraryPickup script when the player interacts with a book.
+	/// </summary>
+	/// <param name="clip">The clip to be played.</param>
+	public void PlayBookCollectedSFX(AudioClip clip)
+	{
+		librarySFX.PlayOneShot(clip);
+	}
+
+	/// <summary>
+	/// This method is called by the LibraryPickup script when the player interacts with a book. Begins the process of collecting a book.
+	/// </summary>
 	public void BookCollected()
 	{
 		//Check if the player has collected all the books
@@ -143,6 +189,9 @@ public class LibraryMinigame : UdonSharpBehaviour
 		}
 	}
 
+	/// <summary>
+	/// The script's entry point when the player interacts with the library minigame.
+	/// </summary>
 	public override void Interact()
 	{
 		StartMinigame();
