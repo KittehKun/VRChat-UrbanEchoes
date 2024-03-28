5bfce8a39394413666a5194fb1c5bfe774bac79f
Zach H <huntzachary30@gmail.com>
2024-03-27 19:33:59 -0500

    Began work on generic item spawners

---
+++ Utility/ItemSpawner.cs
@@ -0,0 +1,15 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class ItemSpawner : UdonSharpBehaviour
+{
+    
+    private void Start()
+    {
+        
+    }
+}
