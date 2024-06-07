666ed6e92f5dc54c865666888c4ea27486b2d609
Zach H. <huntzachary30@gmail.com>
2024-06-06 19:00:52 -0500

    Began overhauled generic JobScript

---
+++ Job/JobScript.cs
@@ -0,0 +1,54 @@
+﻿//This will act as the generic script that will be used for every single job. It will contain the basic methods that will be used for every job, such as AddMoney, RemoveMoney, Energy drain. This script will be used as a base for all other job scripts.
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class JobScript : UdonSharpBehaviour
+{
+    [SerializeField] private double baseReward; //Assigned in Unity based on Job | Awarded to the player for completing the job minigame.
+    [SerializeField] private PlayerStats playerStats; //Assigned in Unity | PlayerStats is separate from this script and is used to add money and drain energy from the player.
+    
+    void Start()
+    {
+        if (!playerStats) Debug.LogError("[JOB SCRIPT]: Missing reference to PlayerStats!"); //Check if the PlayerStats reference is missing.
+    }
+
+    /// <summary>
+    /// Adds money to the player's money based on the reward of the job.
+    /// </summary>
+    private void AddMoney()
+    {
+        playerStats.AddMoney(CalculateReward());
+    }
+
+    /// <summary>
+    /// Calculates the full reward for job completion.
+    /// </summary>
+    /// <returns></returns>
+    private double CalculateReward()
+    {
+        return baseReward + (baseReward * Random.Range(0.0f, 1.0f));
+    }
+
+    /// <summary>
+    /// Calculates the energy drain for the player when they complete the job.
+    /// </summary>
+    /// <returns></returns>
+    private float CalculateEnergyDrain()
+    {
+        return Random.Range(0f, 1f);
+    }
+
+    /// <summary>
+    /// Serves as the entry point for the script. This method will be called when the player completes the job minigame.
+    /// </summary>
+    public void CompleteJob()
+    {
+        //This method will be called when the player completes the job minigame.
+		AddMoney();
+		playerStats.DecreaseEnergy(CalculateEnergyDrain());
+    }
+        
+}
